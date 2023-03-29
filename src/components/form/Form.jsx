c5887391ef1a0ab1aa41700a113583fd0155a189
Anatolii Vlasiuk <vlasyukalid@gmail.com>
2023-03-29 11:10:38 +0300

    Markup was finished

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -7,7 +7,8 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import { styled } from '@mui/material/styles';
 import { Button } from '../button/Button';
-import { 
+import { REGEX } from '../../consts/const';
+import {
   styledTextField,
   themeTypography,
   styledBox,
@@ -20,12 +21,97 @@ import {
 } from './Form.styled'
 import './Form.scss';
 
-export const Form = () => {
-  const [value, setValue] = React.useState('');
-  const StyledTextField = styled(TextField)(styledTextField);
+function validateValue(value, str) {
+  const re = new RegExp(str);
+  return re.test(value);
+}
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
+
+export const Form = () => {
+  const [position, setPosition] = React.useState('');
+  const [name, setName] = React.useState('');
+  const [errorName, setErrorName] = React.useState(false);
+  const [email, setEmail] = React.useState('');
+  const [errorEmail, setErrorEmail] = React.useState(false);
+  const [phone, setPhone] = React.useState('');
+  const [errorPhone, setErrorPhone] = React.useState(false);
+  const [fileName, setFileName] = React.useState('');
+  const [fileError, setFileError] = React.useState(false);
+  const [allValuesAreValid, setAllValuesAreValid] = React.useState(false);
+
+  const StyledTextField = styled(TextField)(styledTextField);
+  const handleFileInputChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const img = new Image();
+      img.onload = () => {
+        if (img.width < 70 || img.height < 70) {
+          setFileError(true);
+        } else {
+          setFileError(false);
+        }
+      };
+      img.src = URL.createObjectURL(file);
+      const fileType = file.type;
+      const fileSize = file.size / 1024 / 1024; // Convert to MB
+      if (
+        !(fileType === 'image/jpeg' || fileType === 'image/jpg') ||
+        fileSize > 5
+      ) {
+        setFileName('');
+        setFileError(true);
+      } else {
+        setFileName(file.name);
+        setFileError(false);
+      }
+    } else {
+      setFileName('');
+      setFileError(false);
+    }
+  };
+
+  const handleClickChooseFile = () => {
+    document.getElementById('file-input').click();
+  };
+
+  function handleChangeName(event) {
+    const inputName = event.target.value;
+    setName(inputName);
+    if (!validateValue(inputName, REGEX.NAME)) {
+      setErrorName(true);
+    } else {
+      setErrorName(false);
+    }
+  }
+
+  function handleChangeEmail(event) {
+    const inputEmail = event.target.value;
+    setEmail(inputEmail);
+    if (!validateValue(inputEmail, `${REGEX.EMAIL}`)) {
+      setErrorEmail(true);
+    } else {
+      setErrorEmail(false);
+    }
+  }
+
+  function handleChangePhone(event) {
+    const inputPhone = event.target.value;
+    setPhone(inputPhone);
+    if (!validateValue(inputPhone, `${REGEX.PHONE}`)) {
+      setErrorPhone(true);
+    } else {
+      setErrorPhone(false);
+    }
+  }
+
+  const isAllValuesRight = () => {
+    if (email !== '' && name !== '' && phone !== '' && fileName !== '' && position !== '') {
+      setAllValuesAreValid(true);
+    }
+  }
+
+  const handleChangePosition = (event) => {
+    setPosition(event.target.value);
   };
 
   const handleClickBtnSignUp = () => {
@@ -34,37 +120,52 @@ export const Form = () => {
 
   const theme = createTheme(themeTypography);
 
+  React.useEffect(() => {
+    isAllValuesRight();
+  }, [position, errorEmail, errorName, errorPhone, fileError])
+
   return (
     <form className='form'>
       <h1 className='form__title'>Working with POST request</h1>
       <ThemeProvider theme={theme}>
-        <Box sx={ styledBox }>
+        <Box sx={styledBox}>
           <TextField
+            error={errorName}
             fullWidth
             label='Your name'
             id='your name'
+            value={name}
+            helperText={errorName && 'Please enter a valid name'}
             sx={styledTextFieldInput}
+            onChange={handleChangeName}
           />
           <TextField
+            error={errorEmail}
             fullWidth
             label='Email'
             id='email'
+            value={email}
             sx={styledTextFieldInput}
+            helperText={errorEmail && 'Please enter a valid email'}
+            onChange={handleChangeEmail}
           />
           <TextField
+            error={errorPhone}
             fullWidth
             label='Phone'
             id='phone'
+            value={phone}
             sx={styledTextFieldInputPhone}
-            helperText='+38 (XXX) XXX - XX - XX'
+            onChange={handleChangePhone}
+            helperText={errorPhone ? 'Please enter a valid phone' : '+38 (XXX) XXX - XX - XX'}
           />
           <div className='form__box-radio'>
             <p className='form__subtitle'>Select your position</p>
             <RadioGroup
               aria-labelledby="demo-controlled-radio-buttons-group"
               name="controlled-radio-buttons-group"
-              value={value}
-              onChange={handleChange}
+              value={position}
+              onChange={handleChangePosition}
               sx={styledRadioGroup}
             >
               <FormControlLabel
@@ -91,25 +192,40 @@ export const Form = () => {
                 label='QA'
                 sx={styledFormControlLabelQa}
               />
+              <p className='form__error-radio'>{!position && 'Should choose a position!'}</p>
             </RadioGroup>
           </div>
           <div className='form__box-upload'>
-            <div className='form__button-upload'>
+            <div 
+              id='file-upload'
+              className='form__button-upload'
+              onClick={handleClickChooseFile}
+            >
               <span className='form__text-button'>Upload</span>
+              <input
+                id='file-input'
+                type='file'
+                accept=".jpg,.jpeg"
+                style={{ display: 'none' }}
+                onChange={handleFileInputChange}
+              />
             </div>
-            <StyledTextField
-              fullWidth
-              label='Upload your photo'
-              disabled
-              id='email'
-              sx={styledFormControlLabelQa}
-            />
+              <StyledTextField
+                error={fileError}
+                fullWidth
+                label='Upload your photo'
+                disabled
+                value={fileName}
+                id='email'
+                sx={styledFormControlLabelQa}
+                helperText={fileError && 'Minimum 70x70px.Format must be jpeg/jpg. Size must not be greater than 5 Mb.'}
+              />
           </div>
           <Button
             type="button"
             onClick={handleClickBtnSignUp}
             name="Sign up"
-            disabled={true}
+            disabled={!allValuesAreValid}
           />
         </Box>
       </ThemeProvider>
--- src/consts/const.js
+++ src/consts/const.js
@@ -1,3 +1,12 @@
 export const BASE_URL = 'https://frontend-test-assignment-api.abz.agency/api/v1';
 export const BORDER_COLOR = '#d0cfcf';
 export const SECONDARY_COLOR = '#00bdd3';
+export const ERROR = '#cb3d40';
+export const REGEX = {
+  // eslint-disable-next-line no-useless-escape
+  EMAIL: '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+  // eslint-disable-next-line no-useless-escape
+  PHONE: '^[\+]{0,1}380([0-9]{9})$',
+  NAME: '^.{2,60}$'
+};
+
